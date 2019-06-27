b789103652de860246040179be5f47a5d87e57fa
josemaria inga villafuerte <chemalonso96@gmail.com>
2019-06-27 06:09:06 -0500

    commit 1

---
+++ Hospital.Repositoy/Migrations/20190625053519_init.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hospital.Repositoy.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombres = table.Column<string>(maxLength: 50, nullable: false),
+                    Apellidos = table.Column<string>(nullable: false),
+                    Dni = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consultas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descripcion = table.Column<string>(nullable: true),
+                    PacienteId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ordenes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OrdenNro = table.Column<string>(nullable: true),
+                    PagoMetodo = table.Column<string>(nullable: true),
+                    Total = table.Column<decimal>(nullable: false),
+                    PacienteId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ordenes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ordenes_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetalleOrdenes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cantidad = table.Column<int>(nullable: false),
+                    OrdenId = table.Column<int>(nullable: false),
+                    MedicamentoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetalleOrdenes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetalleOrdenes_Medicamentos_MedicamentoId",
+                        column: x => x.MedicamentoId,
+                        principalTable: "Medicamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetalleOrdenes_Ordenes_OrdenId",
+                        column: x => x.OrdenId,
+                        principalTable: "Ordenes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_PacienteId",
+                table: "Consultas",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleOrdenes_MedicamentoId",
+                table: "DetalleOrdenes",
+                column: "MedicamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetalleOrdenes_OrdenId",
+                table: "DetalleOrdenes",
+                column: "OrdenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordenes_PacienteId",
+                table: "Ordenes",
+                column: "PacienteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consultas");
+
+            migrationBuilder.DropTable(
+                name: "DetalleOrdenes");
+
+            migrationBuilder.DropTable(
+                name: "Medicamentos");
+
+            migrationBuilder.DropTable(
+                name: "Ordenes");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+        }
+    }
+}
